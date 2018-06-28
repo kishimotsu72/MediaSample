07e84a2a3e5ec37c61439448821bacaa308a634c
kishimotsu72 <kishimotsu72@gmail.com>
2018-06-28 09:49:34 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/mediasample/MediaControlActivity.java
@@ -0,0 +1,13 @@
+dpackage com.websarva.wings.android.mediasample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MediaControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_control);
+    }
+}
