7e87522a36bfbb49287477d84257b79574297b0f
kishimotsu72 <kishimotsu72@gmail.com>
2018-06-28 10:40:15 +0900

    修正

--- app/src/main/java/com/websarva/wings/android/mediasample/MediaControlActivity.java
+++ app/src/main/java/com/websarva/wings/android/mediasample/MediaControlActivity.java
@@ -1,7 +1,22 @@
-dpackage com.websarva.wings.android.mediasample;
+package com.websarva.wings.android.mediasample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MediaControlActivity extends AppCompatActivity {
 
